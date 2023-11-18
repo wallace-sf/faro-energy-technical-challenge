e21c8eb1f059d50e9846f077a92b50a7379ff2b7
Wallace Ferreira <wallaceedua@gmail.com>
2023-11-18 17:14:30 -0300

    ci: setup tailwind + flowbite ui (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import './index.css';
+
 function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <h1>Faro Energy Technical Challenge</h1>
+      <h1 className="text-3xl font-bold underline">
+        Faro Energy Technical Challenge
+      </h1>
+      <img
+        className="w-10 h-10 rounded-full"
+        src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+        alt="Rounded avatar"
+      />
+      <img
+        className="w-10 h-10 rounded"
+        src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+        alt="Default avatar"
+      />
     </>
   );
 }
