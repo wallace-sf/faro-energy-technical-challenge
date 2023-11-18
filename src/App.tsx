ce0f8c4c8d7234e94c21c257b0776e415c9b6410
Wallace Ferreira <wallaceedua@gmail.com>
2023-11-18 16:05:44 -0300

    ci: setup lint + prettier + lint staged + husky (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 function App() {
-
   return (
     <>
       <h1>Hello World</h1>
+      <h1>Faro Energy Technical Challenge</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
