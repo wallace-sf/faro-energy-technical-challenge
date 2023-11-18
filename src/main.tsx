22cc7ac5e33b1debc3cd1084f109ef76b7259db3
wallace-sf <wallaceedua@gmail.com>
2023-11-18 16:04:38 -0300

    ci: setup lint + prettier + lint staged + husky

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import * as React from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import * as ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = document.getElementById('root') as HTMLElement;
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true
-  }
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      open: true,
+    },
+    define: {
+      'process.env': loadEnv(mode, process.cwd()),
+    },
+  };
+});
