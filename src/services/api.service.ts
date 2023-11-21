9a50ef75074fda931d657a5fbefa4bf9c842d026
Wallace Ferreira <wallaceedua@gmail.com>
2023-11-20 22:17:46 -0300

    refactor: add production endpoint (#14)

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: import.meta.env.PROD
+    ? 'https://json-server-power-plants.vercel.app'
+    : 'http://localhost:3333',
   headers: {
     'Content-Type': 'application/json',
   },
